401c93881a90811c22cf1dadb41b118baebf67fb
Kaan BOKE <51021282+kb1907@users.noreply.github.com>
2022-06-06 12:55:32 +0300

    Fisrt commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+app = Flask(__name__)
+model = pickle.load(open('catboost_model-2.pkl', 'rb'))
+
+
+@app.route('/', methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        Age = int(request.form['Age'])
+        #Sex = request.form['Sex']
+        # if(Sex == 'Male'):
+        #   Sex = 'M'
+        # elif(Sex == 'Female'):
+        #   Sex = 'F'
+        RestingBP = int(request.form['RestingBP'])
+        Cholesterol = int(request.form['Cholesterol'])
+        Oldpeak = float(request.form['Oldpeak'])
+        FastingBS = int(request.form['FastingBS'])
+        MaxHR = int(request.form['MaxHR'])
+        prediction = model.predict([[Age, RestingBP, Cholesterol, Oldpeak, FastingBS,
+                                   MaxHR]])
+
+        if prediction == 0:
+            return render_template('index.html', prediction_texts="You are well. No worries")
+        else:
+            return render_template('index.html', prediction_text="Kindly make an appointment with the doctor.")
+    else:
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
